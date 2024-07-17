0818a666b0f1c993732eedbeb5a73770b3c838a3
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-07-17 18:55:36 +0300

    Тест

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -2,8 +2,8 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.*;
 import static io.qameta.allure.Allure.step;
@@ -11,26 +11,31 @@ import static io.qameta.allure.Allure.step;
 public class SearchTests extends TestBase {
 
     @Test
-    void successfulSearchTest() {
-        step("Type search", () -> {
-            $(accessibilityId("Search Wikipedia")).click();
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+    void gettingStartedTest() {
+        step("Первый экран и клик", () -> {
+            $(id("org.wikipedia.alpha:id/fragment_onboarding_forward_button"))
+                    .click();
+            $(id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Новые способы исследований"));
         });
-        step("Verify content found", () ->
-                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(sizeGreaterThan(0)));
-    }
-
-
-    @Test
-    void successfulOpenItemOfWikiTest() {
-        step("Type search", () -> {
-            $(accessibilityId("Search Wikipedia")).click();
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        step("Второй экран и клик", () -> {
+            $(id("org.wikipedia.alpha:id/fragment_onboarding_forward_button"))
+                    .click();
+            $(id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Списки для чтения с синхронизацией"));
         });
-        step("open item Appium", () ->
-                $(id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .click());
-        $(id("org.wikipedia.alpha:id/view_wiki_error_text")).shouldHave(text("Error"));
+        step("Третий экран и клик", () -> {
+            $(id("org.wikipedia.alpha:id/fragment_onboarding_forward_button"))
+                    .click();
+            $(id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Данные и конфиденциальность"));
+        });
+        step("Четвертый экран и клик", () -> {
+            $(id("org.wikipedia.alpha:id/fragment_onboarding_done_button"))
+                    .click();
+            $(id("org.wikipedia.alpha:id/search_container"))
+                    .shouldHave(visible);
+        });
+
     }
 }
